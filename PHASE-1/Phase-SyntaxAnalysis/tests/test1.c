4b1ab886e87193c225bc63c290e9129fa89d66d0
K. Sreesh Reddy <reddy.sreesh224@gmail.com>
2021-11-17 12:10:06 +0530

    Uploaded both phases

---
+++ PHASE-1/Phase-SyntaxAnalysis/tests/test1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int n,i;
+    char ch;
+
+    for (i=0;i<n;i++){
+        if(i<10){
+            int x;
+            while(x<10){
+                x++;
+            }
+        }
+
+    }
+    
+    
+}
